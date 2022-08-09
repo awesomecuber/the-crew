7ba7dbf79c2bf8e74a20fcee77146cb4eda0f560
nbrass <nbrass@uw.edu>
2022-08-08 23:09:51 -0700

    more stuff

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -2,12 +2,12 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-    "prettier",
-  ],
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript/recommended",
+        "@vue/eslint-config-prettier",
+        "prettier",
+    ],
 };
--- front/src/plugins/vuesocket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-export default {
-    install: (app, { connection, options }) => {
-        const socket = io(connection, options);
-    },
-};
